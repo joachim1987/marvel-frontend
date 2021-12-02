6a38a2c0e30a30bee7845269d1f6c28511b7f99f
joachim1987 <bothua.joachim@live.fr>
2021-12-02 18:16:04 +0100

    last commit

--- src/pages/Comics.js
+++ src/pages/Comics.js
@@ -8,6 +8,20 @@ const Comics = () => {
   const [page, setPage] = useState(1)
   const [searchtitle, setSearchtitle] = useState('')
 
+  useEffect(() => {
+    if (data) {
+      data.results.sort((a, b) => {
+        if (a.toLowerCase() < b.toLowerCase()) {
+          return -1
+        } else if (a.toLowerCase() > b.toLowerCase()) {
+          return 1
+        } else {
+          return 0
+        }
+      })
+    }
+  }, [data])
+
   const sortedData = data.results.sort((a, b) => {
     if (a.toLowerCase() < b.toLowerCase()) {
       return -1
